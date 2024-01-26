315a9b434c59eacadaa8bfa4cf413b78a12993d0
DilAfroze <46922893+DilAfroze@users.noreply.github.com>
2024-01-26 22:39:41 +0000

    update requirements and DB connections

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 # Creating a flask object
 app = Flask(__name__)
@@ -17,6 +17,12 @@ def list_jobs():
 	jobs = load_jobs_from_db()
 	return jsonify(jobs)
 
+# <id> will be used for the url for each member. id must be passed to the function.
+@app.route("/job/<id>")
+def show_job(id):
+	job = load_job_from_db(id)
+	return jsonify(job)
+	
 # using our flask object to run our app
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -19,3 +19,13 @@ def load_jobs_from_db():
       jobs.append(row._asdict())
 
     return jobs
+
+def load_job_from_db(id):
+	with engine.connect() as conn:
+		# String formating for query
+		result = conn.execute(text("SELECT * FROM jobs WHERE id = :id"), {'id': id})
+		rows = result.all()
+		if len(rows) == 0:
+			return None
+		else:
+			return dict(rows[0])
